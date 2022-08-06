f8f97b1193d1557983b21d9dbb01b473cbea21d9
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2022-08-06 07:50:28 +0530

    cors url moved to env file

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,8 @@ cloudinary.config({
 
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "http://localhost:3000"],
+    // origin: ["http://localhost:3000/", "http://localhost:3000"],
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
