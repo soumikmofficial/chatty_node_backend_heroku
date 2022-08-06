8a588ebb7a5f2a3e065b9c5ce53f68314a0e5886
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2022-08-06 06:48:15 +0530

    port env added

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.lvw1k.mongodb.net/`;
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SERVER_PORT = process.env.PORT || 5000;
 
 export const config = {
   mongo: {
